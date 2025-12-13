55ba1aa990ef6808c392389fdf221ca50b6a9e5d
Gautam99-coder <gautamtharu1000@gmail.com>
2025-12-14 00:00:48 +0530

    Add basic arithmetic functions for calculator application

---
+++ js-project-1-calculator/script.js
@@ -0,0 +1,25 @@
+function add(){
+    let a=Number(document.getElementById("num1").value);
+    let b=Number(document.getElementById("num2").value);
+    document.getElementById("result").innerText="Result: "+(a+b);
+}
+function subtract(){
+    let a=Number(document.getElementById("num1").value);
+    let b=Number(document.getElementById("num2").value);
+    document.getElementById("result").innerText="Result: "+(a-b);
+}
+function multiply(){
+    let a=Number(document.getElementById("num1").value);
+    let b=Number(document.getElementById("num2").value);
+    document.getElementById("result").innerText="Result: "+(a*b);
+}
+function divide(){
+    let a=Number(document.getElementById("num1").value);
+    let b=Number(document.getElementById("num2").value);
+    if(b===0){
+        alert("cannot divide by zero");
+        return;
+    }
+    document.getElementById("result").innerText="Result: "+(a/b);
+}
+

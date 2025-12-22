a6bd17c168747df7f6db512f858404fea7919a81
Gautam99-coder <gautamtharu1000@gmail.com>
2025-12-22 23:31:00 +0530

    Add menu toggle and modal functionality to UI Master application

---
+++ ui-master/script.js
@@ -0,0 +1,11 @@
+function toggleMenu() {
+  document.getElementById("navMenu").classList.toggle("show");
+}
+
+function openModal() {
+  document.getElementById("modal").style.display = "flex";
+}
+
+function closeModal() {
+  document.getElementById("modal").style.display = "none";
+}

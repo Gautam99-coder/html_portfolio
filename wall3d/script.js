4a738899eb01fb1e1dc118bea771e6430d7304c2
Gautam99-coder <gautamtharu1000@gmail.com>
2025-12-22 23:44:00 +0530

    Refactor wallpaper loading function to use Picsum API and add lazy loading for images

--- wall3d/script.js
+++ wall3d/script.js
@@ -4,19 +4,24 @@ function loadWallpapers(type) {
   grid.innerHTML = "";
 
   for (let i = 0; i < 9; i++) {
+
+    // RANDOM IMAGE ID (IMPORTANT)
+    const randomId = Math.floor(Math.random() * 1000);
+
+    // SIZE BASED ON DEVICE
     const imgUrl =
       type === "phone"
-        ? `https://source.unsplash.com/400x800/?mobile,wallpaper,${Math.random()}`
-        : `https://source.unsplash.com/800x500/?laptop,wallpaper,${Math.random()}`;
+        ? `https://picsum.photos/400/800?random=${randomId}`
+        : `https://picsum.photos/800/500?random=${randomId}`;
 
     const card = document.createElement("div");
     card.className = "card";
 
     card.innerHTML = `
-      <img src="${imgUrl}">
+      <img src="${imgUrl}" loading="lazy">
       <div class="actions">
         <span class="material-icons">3d_rotation</span>
-        <a href="${imgUrl}" download>Download</a>
+        <a href="${imgUrl}" target="_blank" download>Download</a>
       </div>
     `;
 
@@ -24,4 +29,5 @@ function loadWallpapers(type) {
   }
 }
 
+// LOAD DEFAULT
 loadWallpapers("phone");
